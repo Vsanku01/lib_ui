1cadad0c0df1a39a925c613db6515ba4d73690cb
Vsanku <ethhack001@gmail.com>
2019-10-02 18:35:20 +0530

    Added Stream Builder

--- lib/test.dart
+++ lib/test.dart
@@ -1,28 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:convert';
+import 'dart:async';
+import 'package:flutter/services.dart';
+
 
 final _firestore = Firestore.instance;
+var messages;
+var message;
+
+
 
 
 
 class Hai {
   String link1;
   String link2;
+  String link3;
+  String link4;
 
   Hai(this.link1, this.link2);
 
   Hai.fromJson(Map<String, dynamic> json) {
     link1 = json['link1'];
     link2 = json['link2'];
+    link3 = json['link3'];
+    link4 = json['link4'];
+
+
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['link1'] = this.link1;
     data['link2'] = this.link2;
-    print(data['link1']);
-    print('HAiiii');
     return data;
   }
 }
@@ -47,19 +58,6 @@ class _TestPageState extends State<TestPage> {
 
 
 
-
-
-
-
-
-   var messages;
-   var message;
-
-
-
-List url_list = [];
-
-
   Future getUrl () async {
 
      messages = await _firestore.collection('URL').getDocuments();
@@ -67,25 +65,18 @@ List url_list = [];
 
      print(message.data);
 
+
     }
   }
-
-
 
 
 
   @override
   void initState() {
     // TODO: implement initStates
-    getUrl().then((_) {
+    getUrl();
 
-      setState(() {
-        Hai(message.data['link1'],message.data['link2']);
-
-      });
-
-    },);
-
+    
 
 
   }
@@ -102,15 +93,24 @@ List url_list = [];
           appBar: AppBar(
             title: Text('Welcome'),
           ),
-          body: GridView.count(
-// Create a grid with 2 columns. If you change the scrollDirection to
-// horizontal, this produces 2 rows.
-            crossAxisCount: 2,
-// Generate 100 widgets that display their index in the List.
-            children: List.generate(url_list.length, (index) {
-              return bookCard(Color(0xff54A6FF),Color(0xffFFB792),context);
-            }),
-          ),
+          body: StreamBuilder(
+            stream: Firestore.instance.collection('URL').snapshots(),
+            builder: (context,snapshot) {
+              if(!snapshot.hasData) return const Text('Loading');
+              return GridView.builder(
+                itemCount: snapshot.data.documents.length,
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200,
+                    //For 200 width we want 300 height
+                    childAspectRatio: 3 / 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context,index ) =>  bookCard(Color(0xff54A6FF),Color(0xffFFB792),context),
+
+              );
+            },
+          )
         ),
       );
   }
